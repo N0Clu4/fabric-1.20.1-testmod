d4c45e00c4f22096bd419a4b94670aeb95f89def
N0Clu4 <davidgenis100@gmail.com>
2024-01-24 23:05:58 +0000

    Initial commit

---
+++ src/main/java/net/n0clu4/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.n0clu4.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
